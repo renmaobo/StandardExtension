40af1064ba5b94956e567b9644121e3f615138cc
renmaobo <renmaobo@outlook.com>
2022-09-20 22:42:43 +0800

    新增字节扩展类
    1、新增字节扩展类中字节数组转换为字符串
    2、向字符串扩展类中增加转换为整型方法

--- src/System.Extensions/StringExtension.cs
+++ src/System.Extensions/StringExtension.cs
@@ -31,28 +31,28 @@ namespace System
         /// 空或空值
         /// </summary>
         /// <param name="value">待判定字符串</param>
-        /// <returns></returns>
+        /// <returns>结果</returns>
         public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
 
         /// <summary>
         /// 不会空或空值
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">待判定字符串</param>
+        /// <returns>结果</returns>
         public static bool IsNotNullOrEmpty(this string value) => !value.IsNullOrEmpty();
 
         /// <summary>
         /// 空或空白空间
         /// </summary>
         /// <param name="value">待判定字符串</param>
-        /// <returns></returns>
+        /// <returns>结果</returns>
         public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
 
         /// <summary>
         /// 转换成布尔值
         /// </summary>
         /// <param name="value">待转换字符串</param>
-        /// <returns></returns>
+        /// <returns>布尔值结果</returns>
         public static bool ToBoolean(this string value) => !Regex.IsMatch(value, "(false)|0", RegexOptions.IgnoreCase);
 
         /// <summary>
@@ -60,8 +60,38 @@ namespace System
         /// </summary>
         /// <param name="value">待转换字符串</param>
         /// <param name="encoding">编码类型</param>
-        /// <returns></returns>
+        /// <returns>字节字符串</returns>
         public static byte[] ToBytes(this string value, Encoding encoding) => encoding.GetBytes(value);
+
+        /// <summary>
+        /// 转换为整型
+        /// </summary>
+        /// <param name="value">待转换值</param>
+        /// <returns>转换结果</returns>
+        public static int ToInt(this string value) => int.Parse(value);
+
+        /// <summary>
+        /// 转换为整型并判定是否转换成功
+        /// </summary>
+        /// <param name="value">待转换字符串</param>
+        /// <param name="result">结果值</param>
+        /// <returns>转换是否成功结果</returns>
+        public static bool TryToInt(this string value, out int result) => int.TryParse(value, out result);
+
+        /// <summary>
+        /// 转换为长整型
+        /// </summary>
+        /// <param name="value">待转换字符串</param>
+        /// <returns>转换结果</returns>
+        public static long ToLong(this string value) => long.Parse(value);
+
+        /// <summary>
+        /// 转换为长整型并判定是否转换成功
+        /// </summary>
+        /// <param name="value">待转换字符串</param>
+        /// <param name="result">结果值</param>
+        /// <returns>转换是否成功结果</returns>
+        public static bool TryToLong(this string value, out long result) => long.TryParse(value, out result);
     }
 }
 

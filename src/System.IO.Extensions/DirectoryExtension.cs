03ceacaef2174d14beec9ef531243cd5a93d11d8
renmaobo <renmaobo@outlook.com>
2022-09-26 23:16:05 +0800

    新增系统IO扩展类库
    新增目录、文件、路径相关的基础部分扩展方法

---
+++ src/System.IO.Extensions/DirectoryExtension.cs
@@ -0,0 +1,55 @@
+﻿// ============================================================
+// Copyright (C) 2022 RenMaoBo All Rights Reserved.
+// FileName:    	DirectoryExtension
+// Version:       	$V1.0.0.0
+// Create By:   	maobo
+// Email:           renmaobo@outlook.com
+// Create Time:  	2022/9/26 下午11:06
+//
+// Description:
+//
+// ============================================================
+// Modify Mark
+// Modify Time:  2022/9/26 下午11:06
+// Modify By:    maobo
+// Version:      V1.0.0.0
+// Description:
+//
+// ============================================================
+using System;
+namespace System.IO
+{
+    /// <summary>
+    /// 文件目录扩展
+    /// </summary>
+    public static class DirectoryExtension
+    {
+        /// <summary>
+        /// 判定是否存在目录
+        /// </summary>
+        /// <param name="directoryPath">目录路径</param>
+        /// <returns></returns>
+        public static bool ExistDirectory(this string directoryPath) => Directory.Exists(directoryPath);
+
+        /// <summary>
+        /// 删除目录
+        /// </summary>
+        /// <param name="directoryPath">目录路径</param>
+        public static void DeleteDirectory(this string directoryPath) => Directory.Delete(directoryPath);
+
+        /// <summary>
+        /// 获取当前目录下的文件列表
+        /// </summary>
+        /// <param name="directoryPath">目录路径</param>
+        /// <returns></returns>
+        public static string[] GetFiles(this string directoryPath) => Directory.GetFiles(directoryPath);
+
+        /// <summary>
+        /// 获取当前目录的父级目录信息
+        /// </summary>
+        /// <param name="directoryPath">目录路径</param>
+        /// <returns></returns>
+        public static DirectoryInfo GetParentDirectory(this string directoryPath) => Directory.GetParent(directoryPath);
+    }
+}
+
